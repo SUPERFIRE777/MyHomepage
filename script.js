b008da4b8bef891675501add740b77a99b3d0732
SUPERFIRE777 <superfireinfinity@yahoo.co.jp>
2024-09-18 23:50:01 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const navHTML = `
+<nav>
+    <ul>
+        <li><a href="index.html">ホーム</a></li>
+        <li><a href="introduction.html">自己紹介</a></li>
+        <li><a href="works.html">作品</a></li>
+    </ul>
+</nav>
+`;
+
+const videoNames = ["足を拝借.mp4"];
+
+window.onload = function() {
+    document.getElementById('nav-container').innerHTML = navHTML;
+    for(let i=0; i<videoNames.length; i++){
+        document.getElementById(`video${i}`).innerHTML += `
+        <video width="640" height="360" controls>
+            <source src="${videoNames[i]}" type="video/mp4">
+        </video>
+        `;
+    }
+};
