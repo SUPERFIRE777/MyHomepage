5a675e2453a5ac94e93955e7ed29b35923f48e02
SUPERFIRE777 <superfireinfinity@yahoo.co.jp>
2024-09-20 20:27:59 +0900

    表示追加
    ・「現在時刻」と「今日の素因数分解」の表示を追加

--- script.js
+++ script.js
@@ -10,6 +10,51 @@ const navHTML = `
 
 const videoNames = ["足を拝借.mp4"];
 
+function prime_factorize(num) {
+    factor = {};
+    divisor = 2;
+    while(num > 1){
+        if(num % divisor == 0){
+            factor[divisor] = (factor[divisor] || 0) + 1;
+            num /= divisor;
+        }else{
+            divisor++;
+        }
+        if(divisor * divisor > num) break;
+    }
+    if(num > 1) factor[num] = (factor[num] || 0) + 1;
+    console.log(factor);
+    texts = [];
+    upper = "⁰¹²³⁴⁵⁶⁷⁸⁹";
+    for (const [prime, power] of Object.entries(factor)) {
+        text = `${prime}`;
+        if(power > 1){
+            p = power;
+            power_text = "";
+            while(p > 0){
+                power_text += upper[Math.floor(p % 10)];
+                p = Math.floor(p / 10);
+            }
+            power_text = power_text.split("").reverse().join("");
+            text += power_text;
+        }
+        texts.push(text);
+    }
+    return texts.join("×");
+}
+
+function getYYYYMMDD() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    return `${year}${month}${day}`;
+}
+
+function todays_factor(){
+    return prime_factorize(parseInt(getYYYYMMDD()));
+}
+
 window.onload = function() {
     document.getElementById('nav-container').innerHTML = navHTML;
     for(let i=0; i<videoNames.length; i++){
@@ -23,4 +68,16 @@ window.onload = function() {
         </video>
         `;
     }
+    element = document.getElementById('factor');
+    if(element != null){
+        element.innerText = getYYYYMMDD() + "=" + todays_factor();
+    }
+
+    setInterval(function(){
+        var element = document.getElementById('time');
+        if(element != null){
+            element.innerText = new Date().toLocaleString({"hour12": false});
+        }
+    }, 0);
 };
+
